31e0abd29387be5ea2c9381a967ea815c0ccb91a
index003 <index003@sina.cn>
2019-09-04 11:51:37 +0800

    加入购物车和立即购买，弹出模态登录

--- tmall_springboot/src/main/java/com/how2java/tmall/web/ForeRESTController.java
+++ tmall_springboot/src/main/java/com/how2java/tmall/web/ForeRESTController.java
@@ -105,5 +105,13 @@ public class ForeRESTController {
  
         return Result.success(map);
     }
+    
+    @GetMapping("forecheckLogin")
+    public Object checkLogin( HttpSession session) {
+        User user =(User)  session.getAttribute("user");
+        if(null!=user)
+            return Result.success();
+        return Result.fail("未登录");
+    }
  
 }
