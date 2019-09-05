8e4757d0095d00ab68b9c3cd764373aca21cf98d
index003 <index003@sina.cn>
2019-09-05 16:46:41 +0800

    其他拦截器

--- tmall_springboot/src/main/java/com/how2java/tmall/config/WebMvcConfigurer.java
+++ tmall_springboot/src/main/java/com/how2java/tmall/config/WebMvcConfigurer.java
@@ -1,6 +1,8 @@
 package com.how2java.tmall.config;
  
 import com.how2java.tmall.interceptor.LoginInterceptor;
+import com.how2java.tmall.interceptor.OtherInterceptor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,14 +13,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 class WebMvcConfigurer extends WebMvcConfigurerAdapter{
      
+	//追加 OtherInterceptor
+    @Bean
+    public OtherInterceptor getOtherIntercepter() {
+        return new OtherInterceptor();
+    }
     @Bean
     public LoginInterceptor getLoginIntercepter() {
         return new LoginInterceptor();
     }
-     
+ 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(getOtherIntercepter())
+                .addPathPatterns("/**");
         registry.addInterceptor(getLoginIntercepter())
-        .addPathPatterns("/**");      
+                .addPathPatterns("/**");
     }
 }
