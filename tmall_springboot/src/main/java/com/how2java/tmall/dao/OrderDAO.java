5343b12837297d4f4367cba3772bdb3f9f57327a
index003 <index003@sina.cn>
2019-09-05 17:40:38 +0800

    生成订单和订单列表操作

--- tmall_springboot/src/main/java/com/how2java/tmall/dao/OrderDAO.java
+++ tmall_springboot/src/main/java/com/how2java/tmall/dao/OrderDAO.java
@@ -8,5 +8,7 @@ import com.how2java.tmall.pojo.Order;
 import com.how2java.tmall.pojo.User;
  
 public interface OrderDAO extends JpaRepository<Order,Integer>{
+	//用来获取那些某个用户的订单，但是状态又不是 "delete" 的订单。 "delete" 是作为状态调用的时候传进来的
+	public List<Order> findByUserAndStatusNotOrderByIdDesc(User user, String status);
 	
 }
--- tmall_springboot/src/main/java/com/how2java/tmall/service/OrderService.java
+++ tmall_springboot/src/main/java/com/how2java/tmall/service/OrderService.java
@@ -3,6 +3,7 @@ package com.how2java.tmall.service;
 import com.how2java.tmall.dao.OrderDAO;
 import com.how2java.tmall.pojo.Order;
 import com.how2java.tmall.pojo.OrderItem;
+import com.how2java.tmall.pojo.User;
 import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,7 +11,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
- 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 		/*
@@ -38,6 +41,7 @@ public class OrderService {
     public static final String delete = "delete";  
      
     @Autowired OrderDAO orderDAO;
+    @Autowired OrderItemService orderItemService;
      
     public Page4Navigator<Order> list(int start, int size, int navigatePages) {
         Sort sort = new Sort(Sort.Direction.DESC, "id");
@@ -66,5 +70,33 @@ public class OrderService {
     public void update(Order bean) {
         orderDAO.save(bean);
     }
+    
+    @Transactional(propagation= Propagation.REQUIRED,rollbackForClassName="Exception")
+    public float add(Order order, List<OrderItem> ois) {
+        float total = 0;
+        add(order);
  
+        if(false)
+            throw new RuntimeException();
+ 
+        for (OrderItem oi: ois) {
+            oi.setOrder(order);
+            orderItemService.update(oi);
+            total+=oi.getProduct().getPromotePrice()*oi.getNumber();
+        }
+        return total;
+    }
+    public void add(Order order) {
+        orderDAO.save(order);
+    }
+ 
+    public List<Order> listByUserWithoutDelete(User user) {
+        List<Order> orders = listByUserAndNotDeleted(user);
+        orderItemService.fill(orders);
+        return orders;
+    }
+ 
+    public List<Order> listByUserAndNotDeleted(User user) {
+        return orderDAO.findByUserAndStatusNotOrderByIdDesc(user, OrderService.delete);
+    }
 }
